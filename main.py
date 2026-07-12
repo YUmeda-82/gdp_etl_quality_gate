7c0a1b9a722dc86bf890d14aa43ab9ccd006eb1f
YUmeda-82 <umedayuri1809@gmail.com>
2026-07-12 20:31:25 -0300

    fix: deleted imports from main function

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from etl.spark_session import get_spark_session
+from etl.extract import fetch_gdp_per_capita
+from etl.load import create_table_bronze_gdp
+from etl.transform import create_table_silver_gdp
+
 if __name__ == "__main__":
     spark = get_spark_session()
 
